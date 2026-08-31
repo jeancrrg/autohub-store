3801d430cf191686f5910219bfc27956c025696e
jeancrrg <jeancrg3232@gmail.com>
2026-08-31 08:55:18 -0300

    feat (api-gateway): Corrigido todas vulnerabilidades de dependências apresentadas pelo snyk

--- backend/api-gateway/src/main/java/com/autohubstore/gateway/exception/GatewayExceptionHandler.java
+++ backend/api-gateway/src/main/java/com/autohubstore/gateway/exception/GatewayExceptionHandler.java
@@ -1,9 +1,8 @@
 package com.autohubstore.gateway.exception;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.boot.webflux.error.ErrorWebExceptionHandler;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,6 +12,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+import tools.jackson.databind.ObjectMapper;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
